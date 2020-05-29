9a7ba4b3e83e29617dc0b28a6cc5dcc5273dc806
Ankiwoong <ankiwoong@gmail.com>
2020-05-29 14:02:51 +0900

    완전수 구하기

---
+++ Chapter_1/03.py
@@ -0,0 +1,13 @@
+inputNum = int(input('숫자를 입력하세요 : '))
+
+result = 0
+
+for i in range(1, inputNum + 1):
+    for j in range(1, i):
+        if i % j == 0:
+            result += j
+
+    if result == i:
+        print('%d' % (result))
+
+    result = 0
